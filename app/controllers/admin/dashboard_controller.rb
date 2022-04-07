27893907b9344273972340466a8b061f0c47ccd8
P4ulH1ll <jsr3b0rn@gmail.com>
2022-04-07 03:16:45 +0000

    adding basic http authentication

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,15 @@
 class Admin::DashboardController < ApplicationController
+  # basic authentication v
+  http_basic_authenticate_with name: "Jungle", password: "book", except: :index
+
+  # def index
+  #   render plain: "Everyone can see me!"
+  # end
+ 
+  # def edit
+  #   render plain: "I'm only accessible if you know the password"
+  # end
+
   def show
   end
 end
