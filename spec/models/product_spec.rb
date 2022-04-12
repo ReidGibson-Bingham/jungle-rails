9d2d9af03e7f305bdbc9b1448082b0b266bec9d3
P4ulH1ll <jsr3b0rn@gmail.com>
2022-04-12 18:10:33 +0000

    making sure i've commited my previous product spec

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  
+  before(:each) do
+    @category = Category.new(name: "Apparel")
+    @product = Product.new(name: "red", price: 30.00, quantity: 3, category: @category) 
+  end
+  describe 'Validations' do
+    it "has a name that is not nil" do
+      expect(@product.name).not_to be(nil)
+    end
+    it "has a price that is truthy" do
+      expect(@product.price).to be_truthy
+    end
+    it "has a quantity that exists" do
+      expect(@product.quantity).to be_truthy
+    end
+    it "has a category" do
+      expect(@product.category).not_to be(nil)
+    end
+  end
+end
